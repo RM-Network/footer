f838e91fe1bdc7fc2d80c8d416b703a463cce156
Noah Rider <media@noahrider.com>
2024-06-04 01:53:19 -0400

    Add files via upload

---
+++ rmnetworkFooter.js
@@ -0,0 +1,77 @@
+// classify variables
+var rmnetworkFooter = document.getElementById("rmnetworkFooter");
+rmnetworkFooter.innerHTML = 
+// footer
+'<footer>' +
+'    <div class="footer-links">' +
+'        <span id="footer-content">' +
+'            <div><a href="https://rmnetwork.us"><p>© RM Network</p></a></div>' +
+'        </span>' +
+'        <span id="footer-links">' +
+'            <div><p>|</p></div>' +
+'            <div><a href="https://youtube.com"><p>YouTube</p></a></div>' +
+'            <div><p>|</p></div>' +
+'            <div><a href="https://instagram.com"><p>Instagram</p></a></div>' +
+'            <div><p>|</p></div>' +
+'            <div><a href="https://tiktok.com"><p>TikTok</p></a></div>' +
+'            <div><p>|</p></div>' +
+'            <div><a href="https://twitter.com"><p>Twitter</p></a></div>' +
+'            <div><p>|</p></div>' +
+'        </span>' +
+'    </div>' +
+'</footer>' +
+// font
+'<link rel="preconnect" href="https://fonts.googleapis.com">' +
+'<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>' +
+'<link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">' +
+'<style>' +
+// css
+'footer {' +
+'    background: linear-gradient(90deg, rgba(57,0,0,1) 0%, rgba(0,0,0,1) 100%);' +
+'    color: white;' +
+'    padding: 10px;' +
+'    height: 75px;' +
+'    position: relative;' +
+'    text-align: center;' +
+'    font-family: "Montserrat", sans-serif;' +
+'    font-weight: 500;' +
+'    font-style: normal;' +
+'}' +
+'' +
+'footer a {' +
+'    color: white;' +
+'    text-decoration: none;' +
+'}' +
+'' +
+'.footer-links {' +
+'    display: inline-block;' +
+'    vertical-align: middle;' +
+'    padding-top: 10px;' +
+'}' +
+'' +
+'#footer-content {' +
+'    margin-right: 50px;' +
+'}' +
+'' +
+'#footer-content div {' +
+'    display: inline-block;' +
+'    margin-right: 40px;' +
+'}' +
+'' +
+'#footer-links div {' +
+'    display: inline-block;' +
+'    margin-left: 20px;' +
+'}' +
+'' +
+'#footer-links a {' +
+'    text-decoration: underline;' +
+'    color: white;' +
+'}' +
+'' +
+'#footer-links a:hover {' +
+'    color: black;' +
+'}' +
+'#rmnetworkFooter {' +
+'    margin: 0px !important;' +
+'};' +
+'</style>';
